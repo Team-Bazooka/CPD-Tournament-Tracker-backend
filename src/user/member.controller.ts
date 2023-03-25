f708864d8fc5125e66a4ccf7d506ce3112592a79
besufikad17 <besufikadtilahun1994@gmail.com>
2023-03-25 17:39:42 +0300

    schema updated

--- src/user/member.controller.ts
+++ src/user/member.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
   HttpException,
   HttpStatus,
@@ -16,6 +17,7 @@ import {
   UserExistsException,
   UserNotFoundExceptions,
 } from 'src/utils/exceptions';
+import { invitation_status } from '@prisma/client';
 import * as jwt from 'jsonwebtoken';
 import * as bcrypt from 'bcryptjs';
 
@@ -80,7 +82,7 @@ export class MemberController {
           fname,
           lname,
           student_id,
-          team_id: 0,
+          team_id: null,
           kattis_acct_link,
           tg_username,
           email,
@@ -279,6 +281,206 @@ export class MemberController {
 
         return { team: newTeam };
       }
+    } catch (error) {
+      console.log(error);
+      if (error instanceof HttpException) {
+        throw new HttpException(error.message, error.getStatus());
+      } else {
+        throw new HttpException(
+          error.meta || 'Error occurred check the log in the server',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+
+  @Post("/add_member/:id")
+  async addTeamMemebers(
+    @Param("id") id: number,
+    @Body() invitationData: { team_name: string, identifier: string }
+  ){
+
+    console.log(invitationData, id);
+    
+    const { team_name, identifier } = invitationData;
+    if(!id || !team_name || !identifier){
+      throw new IncompleteInputException();
+    }
+
+    try {
+      const u = await this.prismaService.member.findMany({
+        where: {
+          OR: [
+            {
+              student_id: identifier
+            },
+            {
+              email: identifier
+            },
+            {
+              tg_username: identifier
+            }
+          ]
+        }
+      })
+
+      if(u[0]){
+        await this.prismaService.invitation.create({
+          data: {
+            member_id: u[0].id,
+            invitor_id: typeof id === "string" ? parseInt(id) : id,
+            team_name
+          }
+        })
+
+        return {
+          message: "Invitation sent!!",
+          code: HttpStatus.OK
+        }
+      }else{
+        throw new UserNotFoundExceptions();
+      }
+    } catch (error) {
+      console.log(error);
+      if (error instanceof HttpException) {
+        throw new HttpException(error.message, error.getStatus());
+      } else {
+        throw new HttpException(
+          error.meta || 'Error occurred check the log in the server',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+
+  @Get("/notifications/:id")
+  async getNotifications(
+    @Param("id") id : number
+  ){
+    if(!id){
+      throw new IncompleteInputException()
+    }
+
+    try {
+      const invitations = await this.prismaService.invitation.findMany({
+        where: {
+          member_id: typeof id === "string" ? parseInt(id) : id 
+        }
+      })
+      
+      return { data: invitations, code: HttpStatus.OK }
+    } catch (error) {
+      console.log(error);
+      if (error instanceof HttpException) {
+        throw new HttpException(error.message, error.getStatus());
+      } else {
+        throw new HttpException(
+          error.meta || 'Error occurred check the log in the server',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+
+  @Put("/resolve/:id")
+  async resolveInvitation(
+    @Param("id") id: number,
+    @Body() decisionData : { invitation_id: number, decision: string }
+  ){
+    const { invitation_id, decision } = decisionData;
+    if(!invitation_id || !decision){
+      throw new IncompleteInputException()
+    }
+
+    try {
+      if(decision === "accept"){
+        await this.prismaService.invitation.update({
+          where: {
+            id: invitation_id
+          },
+          data: {
+            status: invitation_status.accepted
+          }
+         })
+
+         const inv = await this.prismaService.invitation.findMany({
+          where: {
+            id: invitation_id
+          }
+         })
+
+         const team = await this.prismaService.team.findMany({
+          where: {
+            name: inv[0].team_name
+          }
+         })
+
+         await this.prismaService.member.update({
+          where: {
+            id: typeof id === "string" ? parseInt(id) : id 
+          },
+          data: {
+            team_id: team[0].id
+          }
+         })
+      }else{
+        await this.prismaService.invitation.update({
+          where: {
+            id: invitation_id
+          },
+          data: {
+            status: invitation_status.declined
+          }
+         })
+      }
+
+      return { message: 'Done!', code: HttpStatus.ACCEPTED };
+    } catch (error) {
+      console.log(error);
+      if (error instanceof HttpException) {
+        throw new HttpException(error.message, error.getStatus());
+      } else {
+        throw new HttpException(
+          error.meta || 'Error occurred check the log in the server',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+  }
+
+  @Put("/update/team/:id")
+  async updateTeamProfile (
+    @Param("id") id: number,
+    @Body() updateData : { name:string, logo_url: string }
+  ){
+    const { name, logo_url } = updateData;
+    if(!id){
+      throw new IncompleteInputException()
+    }
+
+    try {
+       const n = await this.prismaService.team.count({
+        where: {
+          name
+        }
+       })
+
+       const exists = n > 0 ? true : false;
+       if(exists){
+        throw new TeamExistsException()
+       }else{
+        await this.prismaService.team.update({
+          where: {
+            id: typeof id === "string" ? parseInt(id) : id
+          },
+          data: {
+            name,
+            logo_url
+          }
+        })
+
+        return { message: 'Done!', code: HttpStatus.ACCEPTED };
+       }
     } catch (error) {
       console.log(error);
       if (error instanceof HttpException) {
